deb5651dddd8f895d6d8b3a62dc7c1b1897a66eb
Fran <frankarroyop@gmail.com>
2022-01-17 13:06:16 +0100

    Login y Register html creado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+// app.module.ts
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { routing } from "./app-routing.module";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
+import { AppComponent } from "./app.component";
+import { RegisterComponent } from "./register/register.component";
+import { FormsModule } from "@angular/forms";
+import { LoginComponent } from "./login/login.component";
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent,LoginComponent, RegisterComponent],
+  imports: [BrowserModule, routing,FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
